949890dd7b8e4d33a11b2d1a13303f878e704754
Kelly HUYNH <khuynh@e2r11p6.clusters.42paris.fr>
2023-02-12 18:25:17 +0100

    bref

--- MINISHELL/PARSING/SPLITS.c
+++ MINISHELL/PARSING/SPLITS.c
@@ -6,33 +6,11 @@
 /*   By: khuynh <khuynh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 21:22:51 by khuynh            #+#    #+#             */
-/*   Updated: 2023/02/08 23:32:27 by khuynh           ###   ########.fr       */
+/*   Updated: 2023/02/12 18:22:12 by khuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../LIB/minishell.h"
-
-
-void	first_split(t_token *tok, char *cmd)
-{
-	int	i;
-
-	i = 0;
-	while (status(cmd) == DEFAULT)
-	{
-		tok->value = cmd[i];
-		i++;
-	}
-	while (status(cmd) == SINGLE)
-	{
-		remplir tok->value d'un autre node ?? comment faire
-	}
-	while (status(cmd) == DOUBLE)
-	{
-		remplir tok->value d'un autre node ?? comment faire
-	}
-	
-}
 
 int	status(char *cmd)
 {
@@ -46,15 +24,54 @@ int	status(char *cmd)
 		if (cmd[i] == '\"')
 		{
 			while (cmd[i] && cmd[i + 1] != '\"')
+			{
 				status = DOUBLE;
+				i++;
+			}
 		}
 		else if (cmd[i] == '\'')
 		{
 			while (cmd[i] && cmd[i + 1] != '\'')
+			{
 				status = SINGLE;
+				i++;
+			}
 		}
 		i++;
 	}
 	return (status);
 }
 
+char	*first_split(char *cmd)
+{
+	int		i;
+	char	*node;
+
+	i = 0;
+	node = NULL;
+	while (status(cmd) == DEFAULT)
+	{
+		node[i] = cmd [i];
+		i++;
+		return (node);
+	}
+	while (status(cmd) == SINGLE)
+	{
+		node[i] = cmd [i];
+		i++;
+		return (node);
+	}
+	while (status(cmd) == DOUBLE)
+	{
+		node[i] = cmd [i];
+		i++;
+		return (node);
+	}
+	return (node);
+}
+
+int main()
+{
+	char cmd[] = "ls -la | echo \"hi\"";
+	printf("%d\n", status(cmd));
+}
