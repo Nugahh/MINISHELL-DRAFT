3198faa70088fbfbcc3409e49fcd3cdb5663914f
nugahh <fwong@42.student.fr>
2023-02-18 02:03:56 +0100

    help

--- MINISHELL/PARSING/first_split_index.c
+++ MINISHELL/PARSING/first_split_index.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   first_split_index.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khuynh <khuynh@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fwong <fwong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:17:41 by khuynh            #+#    #+#             */
-/*   Updated: 2023/02/18 00:37:28 by khuynh           ###   ########.fr       */
+/*   Updated: 2023/02/18 02:01:39 by fwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,9 +76,10 @@ int	*ft_store_state(char *cmd)
 
 	i = 0;
 	state = DEFAULT;
-	store = calloc(strlen(cmd), sizeof(int));
+	store = calloc(strlen(cmd + 2), sizeof(int));
 	while (cmd[i])
 	{
+		printf("state = %d\n", store[i]);
 		if (cmd[i] == '\'' && state == SINGLE)
 			state = DEFAULT;
 		else if (cmd[i] == '\'' && state == DEFAULT)
@@ -103,25 +104,27 @@ void	ft_split_test(char *cmd, t_token **head)
 
 	i = 0;
 	store = ft_store_state(cmd);
+	for (int x = 0; x < 12; x++)
+		printf("store[%d] = %d\n", x, store[x]);
 	end = 0;
 	while (cmd[i])
 	{
-//			printf("cmd x[%d] = %c\n", i, cmd[i]);
+			// printf("cmd x[%d] = %c\n", i, cmd[i]);
 
-		while (cmd[i] == 32 && store[i] == DEFAULT)
+		while (cmd[i] != '\0' && cmd[i] == 32 && store[i] == DEFAULT)
 		{
+			printf("cmd h[%d] = %c\n", i, cmd[i]);
 			end++;
 			i++;
-			printf("cmd h[%d] = %c\n", i, cmd[i]);
 		}
 		start = i;
 		while (cmd[i] && cmd[i] != 32 && cmd[i] != '<' && cmd[i] != '>' && cmd[i] != '|' && store[i] == DEFAULT)
 		{
+			printf("cmd [%d] = %c\n", i, cmd[i]);
 			i++;
 			end++;
-			printf("cmd [%d] = %c\n", i, cmd[i]);
 		}
-		while (store[i] != DEFAULT)
+		while (store[i] == SINGLE || store[i] == DOUBLE)
 		{
 			printf("cmd[%d] = %c\n", i, cmd[i]);
 			i++;
@@ -137,13 +140,15 @@ void	ft_split_test(char *cmd, t_token **head)
 				i++;
 			}
 		}
-		if (cmd[i] == 32 && store[i] == DEFAULT || (cmd[i + 1] == '\0'))
+		if (cmd[i] && cmd[i] != 32 && cmd[i + 1] == '\0')
+			return ;
+		if (cmd[i] == '\0')
+			insert(head, cmd, start, end - start - 1);
+		if (cmd[i] && cmd[i] == 32 && store[i] == DEFAULT)
 		{
 //			printf("je rentre \n");			
 			insert(head, cmd, start, end - start - 1);
 		}
-		if (cmd[i] == '\0')
-			return;
 		i++;
 		end++;
 	}
@@ -154,10 +159,11 @@ int main(int ac, char **av)
 	t_token *head = NULL;
 	(void)ac;
 	(void)av;
-	ft_split_test("   echo he    llo haha \"lo   l\"     ", &head);
+	ft_split_test("echo \"lo   l\" caca  ", &head);
 	// int *store = ft_store_state("0123 \"\" \'123\"\'");
 	// for (int i = 0; i < 20; i++)
 	// 	printf("%d = %d\n", i, store[i]);
 	printstr(head);
 	return 0;
 }
+//"   echo he    llo haha \"lo   l\"     "
