120f605c3f3f1fc6107bf2c93cecec1ee53ce86a
Kelly HUYNH <khuynh@e1r6p5.clusters.42paris.fr>
2023-02-19 15:37:18 +0100

    lol:

--- MINISHELL/BUILT-IN/echo.c
+++ MINISHELL/BUILT-IN/echo.c
@@ -6,19 +6,36 @@
 /*   By: khuynh <khuynh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 23:50:54 by khuynh            #+#    #+#             */
-/*   Updated: 2023/02/19 00:55:29 by khuynh           ###   ########.fr       */
+/*   Updated: 2023/02/19 15:33:04 by khuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../LIB/minishell.h"
 
-int argc_count(char **str)
+int check_flag(char **str)
 {
+	int	i;
+	int	j;
 	int	count;
-	
+
 	count = 0;
-	while (str[count])
-		count++;
+	j = 0;
+	i = 0;
+	while (str[i] && ft_strlen(str[i]) >= 2)
+	{
+		if(str[i][j] == '-' && str[i][j + 1] == 'n')
+		{
+			j++;
+			while (str[i][j] == 'n')
+				j++;
+			if (j == ft_strlen(str[i]))
+				count++;
+		}
+		else
+			return (count);
+		i++;
+		j = 0;
+	}
 	return (count);
 }
 
@@ -27,27 +44,17 @@ int	ft_echo(char **str)
 	int	i;
 	int	n;
 
+	str++;
 	n = 0;
-	i = 1;
-	if (argc_count(str) > 1)
+	i = check_flag(str);
+	if (i > 0)
+		n++;
+	while (str[i])
 	{
-		while (str[i])
-		{
-			if (ft_strncmp(str[i], "-n", ft_strlen(str[i])) == 0)
-				n = 1;
-			i++;
-		}
-		if (n == 1)
-			i = 2;
-		else
-			i = 1;
-		while (str[i])
-		{
-			printf("%s", str[i]);
-			if (str[i + 1] && str[i][0] != '\0')
-				printf(" ");
-			i++;
-		}
+		printf("%s", str[i]);
+		if (str[i + 1] && str[i][0] != '\0')
+			printf(" ");
+		i++;
 	}
 	if (n == 0)
 		printf("\n");
