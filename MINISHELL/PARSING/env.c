3400b6ed4d557b928accb548a60876d3e2119754
kellycious <kellyhuynh@outlook.fr>
2023-02-20 23:45:16 +0100

    env in process

--- MINISHELL/PARSING/env.c
+++ MINISHELL/PARSING/env.c
@@ -6,7 +6,7 @@
 /*   By: khuynh <khuynh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:08:46 by khuynh            #+#    #+#             */
-/*   Updated: 2023/02/19 19:11:39 by khuynh           ###   ########.fr       */
+/*   Updated: 2023/02/20 23:44:01 by khuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	nb_env(char **envp)
 	return (count);
 }
 
-char **extract_env(char **envp, t_env **head)
+char **extract_env(char **envp)
 {
 	int		count;
 	int		i;
@@ -46,23 +46,28 @@ char **extract_env(char **envp, t_env **head)
 	return (copy);
 }
 
-t_env	*init_env(char *env_name, char *envalue)
+t_env *create_env(char *name, char *value)
 {
 	t_env	*new;
 	
 	new = ft_calloc(1, sizeof(t_env));
-	new->name = env_name;
-	new->value = envalue;
+	if (!new)
+		return (0);
+	new->name = ft_strdup(name);
+	if (value)
+		new->value = ft_strdup(value);
+	else
+		new->value = ft_strdup("");
 	new->next = NULL;
 	return (new);
 }
 
-void	insert_env(t_env **head, char *env_name, char *envalue)
+void	insert_env(t_env **head, char *name, char *value)
 {
-	t_env *new;
-	t_env *temp;
+	t_env	*new;
+	t_env	*temp;
 
-	new = init_env(env_name, envalue);
+	new = create_env(name, value);
 	if (!*head)
 	{
 		*head = new;
@@ -72,6 +77,35 @@ void	insert_env(t_env **head, char *env_name, char *envalue)
 	while (temp->next)
 		temp = temp->next;
 	temp->next = new;
+}
+
+void	env_parser(char **envp, t_env **head)
+{
+	int		i;
+	int		j;
+	char	*name;
+	char	*value;
+	t_env	*env;
+
+	i = 0;
+	while (envp[i])
+	{
+		j = 0;
+		while (envp[i][j])
+		{
+			if (envp[i][j] == '=')
+			{
+				name = ft_substr(envp[i], 0, j);
+				value = ft_substr(envp[i], j + 1, ft_strlen(envp[i]) - j - 1);
+				break;
+			}
+			j++;
+		}
+		insert_env(head, name, value);
+		free(name);
+		free(value);
+		i++;
+	}
 }
 
 int	main(int ac, char **av, char **envp)
