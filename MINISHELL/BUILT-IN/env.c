f7416b7981155178c78ceb214dbf34dfccf7a490
khuynh <khuynh@student.42.fr>
2023-02-21 21:17:31 +0100

    bref

--- MINISHELL/BUILT-IN/env.c
+++ MINISHELL/BUILT-IN/env.c
@@ -9,9 +9,19 @@ int	ft_env(t_env *head, int fd, char **envp)
 	while (temp)
 	{
 		ft_putstr_fd(temp->name, fd);
-		ft_putchar_fd("=", fd);
+		ft_putchar_fd('=', fd);
 		ft_putstr_fd(temp->value, fd);
-		ft_putchar_fd("\n", 1);
+		ft_putchar_fd('\n', 1);
 		temp = temp->next;
 	}
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	(void)ac;
+	(void)av;
+	t_env	*env;
+
+	env = NULL;
+	ft_env(env, 1, envp);
 }
